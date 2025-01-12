ddce676d0199cccec6f99ac3a6629e4e858e09ec
8eigh <randompochta99@mail.ru>
2025-01-12 14:24:42 +0300

    Update metod runge_kutta_solver
    git add core/
    git commit -m "Исправлен метод Рунге-Кутты."
    git push origin main

--- core/runge_kutta_solver.py
+++ core/runge_kutta_solver.py
@@ -5,7 +5,7 @@ class RungeKuttaSolver:
         self.f = f  # Система ОДУ
         self.t_start = t_start
         self.t_end = t_end
-        self.y0 = y0
+        self.y0 = np.array(y0)
         self.h = h
 
     def solve(self):
@@ -14,9 +14,9 @@ class RungeKuttaSolver:
         trajectory = [(t, *y)]
 
         while t < self.t_end:
-            k1 = self.h * np.array(self.f(t, y))
-            k2 = self.h * np.array(self.f(t + self.h / 2, y + k1 / 2))
-            y += k2
+            k1 = self.f(t, y)
+            k2 =self.f(t + self.h*0.5, y + self.h * 0.5 * k1)
+            y += self.h * (1 * k2)
             t += self.h
             trajectory.append((t, *y))
 
